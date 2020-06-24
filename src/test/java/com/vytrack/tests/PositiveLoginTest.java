d6d348b1da0030e411735bee71332d04acc017e1
tr009068 <59310312+yldzali@users.noreply.github.com>
2020-06-24 15:26:05 +0100

    a

--- src/test/java/com/vytrack/tests/PositiveLoginTest.java
+++ src/test/java/com/vytrack/tests/PositiveLoginTest.java
@@ -9,31 +9,37 @@ public class PositiveLoginTest extends TestBase {
 
 
     @Test
-    public void loginAsDriver(){
+    public void loginAsDriver() {
+
+        //name of the test
+        extentLogger = report.createTest("login as Driver");
         LoginPage loginPage = new LoginPage();
 
+        extentLogger.info("Enter username: User10");
         String username = ConfigurationReader.get("driver_username");
+        extentLogger.info("Enter password: UserUser123");
         String password = ConfigurationReader.get("driver_password");
-
-        loginPage.login(username,password);
-
-        Assert.assertEquals(driver.getCurrentUrl(),"https://qa3.vytrack.com/","verify we successfuly login");
-        ;
+        extentLogger.info("click login button");
+        loginPage.login(username, password);
+        extentLogger.info("verify page url");
+        Assert.assertEquals(driver.getCurrentUrl(), "https://qa3.vytrack.com/", "verify we successfuly login");
+        extentLogger.pass("driver test passed");
     }
 
     @Test
-    public void loginAsStoreManager(){
+    public void loginAsStoreManager() {
+        extentLogger = report.createTest("login store manager");
         LoginPage loginPage = new LoginPage();
 
+        extentLogger.info("Enter username: storemanager57");
         String username = ConfigurationReader.get("storemanager_username");
+        extentLogger.info("Enter password: UserUser123");
         String password = ConfigurationReader.get("storemanager_password");
-
-        loginPage.login(username,password);
-
-        Assert.assertEquals(driver.getCurrentUrl(),"https://qa3.vytrack.com/","verify we successfuly login");
-
-
-
+        extentLogger.info("click login button");
+        loginPage.login(username, password);
+        extentLogger.info("verify page url");
+        Assert.assertEquals(driver.getCurrentUrl(), "https://qa3.vytrack.com/", "verify we successfuly login");
+        extentLogger.pass("store manager test passed");
 
     }
 
